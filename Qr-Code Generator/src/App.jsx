c600f304d09253e3824e14a02f9d6cbbb8f0914e
Vrushabh Patil <vrushabhpatil4801@gmail.com>
2024-08-07 00:24:00 +0530

    F

---
+++ Qr-Code Generator/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+import QRcodeGenerator from './Components/QRcodeGenerator'
+
+function App() {
+
+  return (
+    <>
+      <div>
+       <QRcodeGenerator />
+
+      </div>
+    </>
+  )
+}
+
+export default App
