f7b20348b2b8b764aadda72d0094c138271b19a3
Vrushabh Patil <vrushabhpatil4801@gmail.com>
2024-08-07 22:17:57 +0530

    Project Complete

--- Qr-Code Generator/src/Components/QRcodeGenerator.jsx
+++ Qr-Code Generator/src/Components/QRcodeGenerator.jsx
@@ -33,28 +33,32 @@ function QrCodeGenerator() {
   };
 
   return (
-    <div className="qrcode__container">
-      <h1>QR Code Generator</h1>
-      <div className="qrcode__container--parent" ref={qrCodeRef}>
-        <div className="qrcode__input">
+    <div className=" pt-28 pl-96 pr-96 text-center">
+      <h1 className="text-3xl font-bold p-4">QR Code Generator</h1>
+      <div className="p-6 items-center" ref={qrCodeRef}>
+        <div className="pl-40 flex gap-5">
           <input
+          className="p-2 border-2"
             type="text"
             placeholder="Enter a URL"
             value={url}
             onChange={(e) => setUrl(e.target.value)}
           />
         
-          <button onClick={handleQrCodeGenerator}>Generate QR Code</button>
+          <button className="rounded-lg bg-slate-700 p-2 hover:bg-slate-500 text-white" onClick={handleQrCodeGenerator}>Generate QR Code</button>
         </div>
 
         {qrIsVisible && (
-          <div className="qrcode__download">
-            <div className="qrcode__image">
+          <div className="p-7">
+            <div className="pl-40">
               <QRCode value={url} size={300} />
             </div>
-            <button onClick={downloadQRCode}>Download QR Code</button>
+            <div className="pt-8 pb-4">
+            <button className="bg-slate-700 rounded-lg hover:bg-slate-500 text-white p-2" onClick={downloadQRCode}>Download QR Code</button>
+            </div>
           </div>
         )}
+        <h3 className="pt-5 text-2xl font-semibold">Made by Vrushabh Patil</h3>
       </div>
     </div>
   );
